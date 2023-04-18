182d114647141860daecd6503de15922d6bbcd7c
Anastasia <dzheyn.anna@mail.ru>
2023-04-18 15:57:42 +0400

    minor edit

--- project/src/pages/error-page/error-page.tsx
+++ project/src/pages/error-page/error-page.tsx
@@ -1,9 +1,12 @@
 import Logo from '../../components/logo/logo';
 import './error-page.css';
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
+import { AppRoute } from '../../utils/constants';
 
 function ErrorPage():JSX.Element{
+  const location = useLocation();
+  const isErrorOnMainPage = location.pathname === AppRoute.Main;
   return(
     <div className="user-page">
       <Helmet>
@@ -15,8 +18,8 @@ function ErrorPage():JSX.Element{
       </header>
       <div className="error-container user-page__content">
         <h1>404. Page not found</h1>
-
-        <Link to='/'>Go back to main page</Link>
+        { !isErrorOnMainPage &&
+        <Link to='/'>Go back to main page</Link>}
       </div>
 
       <footer className="page-footer">
